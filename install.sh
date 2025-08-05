467599e7cc516619de34608e4fd5017284bd552b
Karokrok <51780505+roymalto@users.noreply.github.com>
2025-08-05 21:59:48 +1200

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+mkdir ~/Projects
+mkdir ~/Projects/ATISHEALTH
+
+git clone https://github.com/roymalto/pinotech-base-build.git
+
+mv pinotech-base-build/* . && mv pinotech-base-build/.* . 2>/dev/null
+rmdir pinotech-base-build
+
+git clone https://github.com/Bahmni/bahmni-docker.git
+cd bahmni-docker/bahmni-standard
+cp ~/Projects/ATISHEALTH/custom-branding/docker-compose.yml ~/Projects/ATISHEALTH/bahmni-docker/bahmni-standard/
+
+cp ~/Projects/ATISHEALTH/custom-branding/.env ~/Projects/ATISHEALTH/bahmni-docker/bahmni-standard/
+docker compose up -d
